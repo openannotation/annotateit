f51bf6b74ddb8e76f92bfd27e142e07b90e99686
Nick Stenning <nick@whiteink.com>
2012-03-07 14:14:38 +0000

    Update dependency on annotator -> v0.6.3

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ setup(
     packages = find_packages(),
 
     install_requires = [
-        'annotator==0.6.2',
+        'annotator==0.6.3',
         'Flask==0.8',
         'Flask-Mail==0.6.1',
         'Flask-SQLAlchemy==0.15',

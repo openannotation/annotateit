070856aa16eeb97307bbce15b733cc52120096e6
Nick Stenning <nick@whiteink.com>
2012-04-25 17:56:00 +0100

    Bump dependency on annotator

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ setup(
     packages = find_packages(),
 
     install_requires = [
-        'annotator==0.7.6',
+        'annotator==0.7.7',
         'Flask==0.8',
         'Flask-Mail==0.6.1',
         'Flask-SQLAlchemy==0.15',

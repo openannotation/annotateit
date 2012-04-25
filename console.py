c2a2e0f3653134456a8b25ef41cfe03d4d5686a1
Nick Stenning <nick@whiteink.com>
2012-04-25 17:55:49 +0100

    Simplify login in console.py

--- console.py
+++ console.py
@@ -2,18 +2,13 @@ from IPython import embed
 
 import annotateit
 from annotateit import model, db, es
+from flask import g
 
 def main():
     app = annotateit.create_app()
 
     with app.test_request_context():
-        from annotator import auth
-        consumer = model.Consumer.fetch('annotateit')
-        token = auth.encode_token({'consumerKey': 'annotateit', 'userId': 'admin'}, consumer.secret)
-        headers = {'x-annotator-auth-token': token}
-
-    # Push new test context with auth headers attached
-    with app.test_request_context(headers=headers):
+        g.user = model.User.fetch('admin')
         embed(display_banner=False)
 
 if __name__ == '__main__':

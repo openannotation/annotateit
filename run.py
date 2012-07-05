74c583761c96ec9bf78f39ca12039cd0316163cd
Nick Stenning <nick@whiteink.com>
2012-07-05 16:00:29 +0100

    Bind to 0.0.0.0 by default

--- run.py
+++ run.py
@@ -3,6 +3,6 @@ import annotateit
 
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))
-    host = os.environ.get('HOST', '127.0.0.1')
+    host = os.environ.get('HOST', '0.0.0.0')
     app = annotateit.create_app()
     app.run(port=port, host=host)

e21dcf9f40af9e701835a4b59d45f1ff9f6bf845
Nick Stenning <nick@whiteink.com>
2012-07-24 22:40:25 +0100

    Add tox config

--- run_tests.py
+++ run_tests.py
@@ -3,12 +3,16 @@ import nose
 
 if __name__ == '__main__':
 
-    env['DEBUG'] = 'True'
-    env['TESTING'] = 'True'
-    env['CSRF_ENABLED'] = 'False'
-    env['DATABASE_URL'] = 'sqlite:///:memory:'
-    env['ELASTICSEARCH_INDEX'] = 'annotator_test'
-    env['SECRET_KEY'] = 'test-random-secret-key'
+    env.update({
+        'DEBUG': 'True',
+        'TESTING': 'True',
+        'CSRF_ENABLED': 'False',
+        'DATABASE_URL': 'sqlite:///:memory:',
+        'ELASTICSEARCH_INDEX': 'annotator_test',
+        'SECRET_KEY': 'test-random-secret-key',
+        'RECAPTCHA_PUBLIC_KEY': 'test-recaptcha-public-key',
+        'RECAPTCHA_PRIVATE_KEY': 'test-recaptcha-private-key'
+    })
 
     nose.run()
 
